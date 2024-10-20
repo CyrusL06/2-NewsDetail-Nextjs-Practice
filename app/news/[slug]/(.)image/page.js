5609d92c2087dcbd634ee35221e22f6b968c51d6
Cyrus Lorenzo <cyruslorenzo0626@gmail.com>
2024-10-19 20:38:16 -0700

    Added Interception Routes

---
+++ app/news/[slug]/(.)image/page.js
@@ -0,0 +1,31 @@
+//Copied the same code as images page.js
+
+import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
+
+//param propertygive acces to slug because nested routes 
+//inside of dynamic route will also have access in this news item slug
+export default function InterceptedImagePage({params}) {
+    // Copying the logic from earlier 
+    const newsSlug = params.slug
+    const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug)
+
+    if (!newsItem) {
+        // Debug: Inform if no item was found
+        console.error("News item not found for slug:", newsSlug);
+        return notFound(); // This triggers the 404 page
+    }
+    
+    return (
+        <>
+        <h2>Intercepted!</h2>
+        <div className="fullscreen-image">
+            <img 
+            // output the actual Image 
+            src={`/images/news/${newsItem.image}`} 
+            alt = {newsItem.title}
+            />
+        </div>
+        </>
+    );
+}
