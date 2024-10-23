08f68de388994ca5605fc41aad5991b50927594f
Cyrus Lorenzo <cyruslorenzo0626@gmail.com>
2024-10-22 17:40:10 -0700

    Server Side Data FEtching THE BEST WAY

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,54 +1,26 @@
-"use client"
+//NExt Js way to fetch data from a back-END
 
 import NewsList from "@/components/main-header/news-list";
-import { useEffect, useState } from "react";
-
-export default function NewsPage(){
-    const [error, setError] = useState();
-    const [isLoading, setIsLoading] = useState(false);
-    const [news, setNews] = useState();
-    // fetching the backend
-    useEffect(() => {
-
-        async function fetchNews() {
-        const response = await fetch('http://localhost:8080/news');
 
 
-            if (!response.ok) {
-                setError('Failed To fetch news.');
-                setIsLoading(false);
-            }
+export default async function NewsPage(){
+            // fetching the backend
+        //u can use fetch in a direct comonent
+    const response = await fetch('http://localhost:8080/news');
 
-            const news = await response.json();
-            setIsLoading(false);
-            setNews(news)
-        }
+    if(!response.ok) {
+        throw new Error('Failed to Fetch')
+    }
 
-       
-        fetchNews();
-    }, []);
+    const news = await response.json();
 
-    
-        if (isLoading) {
-            return <p>Loading...</p>
-        }
-
-        if (error) {
-            return {error}
-        }
-
-        // nothing
-        let newsContent;
-        //or newslist here
-        if (news) {
-            newsContent = <NewsList news={news}/>
-        }
+     let newsContent;
 
     return(
         <>
         <h1>News Page</h1>
         {/* made it a component so its reusable */}
-       {newsContent}
+        <NewsList news={news}/>
         </>
         
     );
